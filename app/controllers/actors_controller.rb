79b2a85c4b1cfc67ed74b9991000df3e31ffe3a0
Johnsylva <90647997+Johnsylva@users.noreply.github.com>
2025-12-07 20:05:17 -0600

    Add basic user authorization

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,5 +1,5 @@
 class ActorsController < ApplicationController
-  before_action :authenticate_user, except: [:index]
+  before_action :authenticate_user, except: [:index, :show]
 
   def index
     @actors = Actor.all
